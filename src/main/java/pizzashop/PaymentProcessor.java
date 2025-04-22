cbf1b8534495db666045ccf6380353886ccd4f51
Sergio Mayombo Serrano <smayoser@myuax.com>
2025-04-22 19:48:36 +0200

    metodos de pago

---
+++ src/main/java/pizzashop/PaymentProcessor.java
@@ -0,0 +1,8 @@
+package pizzashop;
+
+public class PaymentProcessor {
+    public boolean processPayment(double amount) {
+        System.out.println("Procesando pago de: $" + amount);
+        return true;
+    }
+}
